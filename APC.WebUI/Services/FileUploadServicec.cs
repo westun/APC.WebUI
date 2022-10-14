70c45cd3455995eb66bd095ccb291e46204ef1da
Wesley Tunnermann <wesleytunnermann2@hotmail.com>
2022-10-14 02:01:11 -0700

    add azure storage blob nuget package start file upload service

---
+++ APC.WebUI/Services/FileUploadServicec.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace APC.WebUI.Services
+{
+    public class FileUploadServicec
+    {
+        BlobServiceClient blobServiceClient = new BlobServiceClient("");
+
+        public void UploadFile(IBrowserFile browserFile)
+        {
+            var serviceClient = blobServiceClient.GetBlobContainerClient("products");
+
+            //serviceClient.
+
+        }
+    }
+}
