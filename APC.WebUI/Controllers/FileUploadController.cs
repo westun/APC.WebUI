86e97a06f4395292ba76d4b8374c8e92b1fd6a32
Wesley Tunnermann <wesleytunnermann2@hotmail.com>
2022-10-14 15:28:42 -0700

    add file upload api controller.

---
+++ APC.WebUI/Controllers/FileUploadController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APC.WebUI.Controllers
+{
+    [Route("api/fileupload")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetList()
+        {
+            var list = new List<string>() { "One", "TWO", "3", "FOUR!" };
+
+            return this.Ok(list);
+        }
+    }
+
+}
