2d0f787bfc8288f731876a9e363785f7584f1d02
Wesley Tunnermann <wesleytunnermann2@hotmail.com>
2022-10-22 22:47:57 -0700

    Add migration script for adding Recycled column to Products table

---
+++ APC.DAL/Migrations/20221023054154_AddRecycledBitColumnToProductsTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APC.DAL.Migrations
+{
+    public partial class AddRecycledBitColumnToProductsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Recycled",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Recycled",
+                table: "Products");
+        }
+    }
+}
--- APC.DAL/Models/Product.cs
+++ APC.DAL/Models/Product.cs
@@ -11,7 +11,8 @@ namespace APC.DAL.Models
         public string DisplayName { get; set; }
         public string? Description { get; set; }
         public string? ImageUrl { get; set; }
-        
+        public bool Recycled { get; set; }
+
         public ProductCategory Category { get; set; } 
         public ProductType Type { get; set; } 
         public ICollection<ProductAttributeValue> ProductAttributesValues { get; set; } = new List<ProductAttributeValue>();
