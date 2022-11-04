6607bb91c68686e67fde1b2829072b48160662fc
Wesley Tunnermann <wesleytunnermann2@hotmail.com>
2022-11-04 15:10:47 -0700

    Update companyId column for products table to not be null

---
+++ APC.DAL/Migrations/20221104220640_UpdateCompanyIdInProductsToNotBeNull.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APC.DAL.Migrations
+{
+    public partial class UpdateCompanyIdInProductsToNotBeNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Company_CompanyId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Company_CompanyId",
+                table: "Products",
+                column: "CompanyId",
+                principalTable: "Company",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Company_CompanyId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyId",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Company_CompanyId",
+                table: "Products",
+                column: "CompanyId",
+                principalTable: "Company",
+                principalColumn: "Id");
+        }
+    }
+}
--- APC.DAL/Models/Product.cs
+++ APC.DAL/Models/Product.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int CategoryId { get; set; }
         public int TypeId { get; set; }
-        public int? CompanyId { get; set; }
+        public int CompanyId { get; set; }
         public string Name { get; set; }
         public string DisplayName { get; set; }
         public string? Description { get; set; }
